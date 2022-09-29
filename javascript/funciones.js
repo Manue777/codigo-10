4c23916cf83048833ac9b3a954dcadff9778084d
manue777 <lejuene.manu@gmail.com>
2022-09-28 20:46:09 -0500

    javascrip class

---
+++ javascript/funciones.js
@@ -0,0 +1,25 @@
+//Sintaxys: la forma en la ue escribimos el codigo
+
+//function sumar (){
+
+//}
+let n3=30;
+
+function sumar () {
+    //codigo
+    let n1 =10;
+    let n2 =20; 
+    console.log(n1+n2+n3);
+    //toma n3 porque esta encimay declarado de la fucnion , pero no agarra n4 porque esta fuera de su scop.
+}
+
+let n4 = 40;
+
+console.log(n1);
+console.log(n3);
+
+let nombre = "pepe"
+
+
+
+
