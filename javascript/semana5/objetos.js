8982b7a24eb1648e178abcbc0fd6ebce7454917c
manue777 <lejuene.manu@gmail.com>
2022-09-30 23:52:16 -0500

    math and array methods 1

--- javascript/semana5/objetos.js
+++ javascript/semana5/objetos.js
@@ -1,3 +1,4 @@
+// Arreglo para guardar infinitas peronas:
 const personas = [];
 
 function crearPersona() {
@@ -125,11 +126,71 @@ class Person {
 // Funcion para crear varias personas a partir de 1 modelo:
 function crearPersonasClass() {
 	const persona1 = new Person("bruno", 32, "masculino", 179);
-	const persona2 = new Person("linder", 22, "masculino", 170);
+	const persona2 = new Person("linder", 18, "masculino", 170);
 	const persona3 = new Person("elvia", 25, "femenino", 170);
+	const persona4 = new Person("arnold", 20, "masculino", 170);
 	personas.push(persona1);
 	personas.push(persona2);
 	personas.push(persona3);
+	personas.push(persona4);
 
 	console.log(personas);
+}
+
+function crearPersonasCustom() {
+	const nombre = prompt("Nombre: ");
+	const edad = prompt("Edad: ");
+	const genero = prompt("Genero: ");
+	const talla = prompt("Talla: ");
+	const persona = new Person(nombre, edad, genero, talla);
+	// Agregar a la nueva persona al arreglo "personas":
+	personas.push(persona);
+	console.log(personas);
+}
+
+function notificacion() {
+	window.alert("Soy una notificacion");
+	console.log(window.location.host);
+	console.log(window.location.pathname);
+	console.log(window.innerHeight);
+	console.log(window.innerWidth);
+
+	// window.open("variables.html", "VariablesTab", "popup");
+}
+
+function goVariables() {
+	window.location.href = "variables.html";
+}
+
+function edadMinima() {
+	// Recorrer el arreglo de personas y determinar la edad minima.
+	let edadMinima = 100;
+
+	// personas.forEach((persona) => {
+	// 	if (persona.years < edadMinima) {
+	// 		edadMinima = persona.years;
+	// 	}
+	// });
+	personas.forEach((persona) => {
+		edadMinima = Math.min(edadMinima, persona.years);
+	});
+
+	console.log("Edad Minina", edadMinima);
+}
+
+function buscarPersonaNombre() {
+	const term = prompt("Ingresa el nombre a buscar:");
+
+	let persona_match;
+	personas.forEach((persona) => {
+		if (persona.name === term) {
+			persona_match = persona;
+		}
+	});
+
+	console.log(persona_match);
+
+	// ahora lo hacemos con .find()
+	const persona_find = personas.find((persona) => persona.name === term);
+	console.log(persona_find);
 }
