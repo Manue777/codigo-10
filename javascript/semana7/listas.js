dbfb08254a15a64b1185985a89da382d9236f59f
manue777 <lejuene.manu@gmail.com>
2022-10-11 17:26:07 -0500

    repaso

---
+++ javascript/semana7/listas.js
@@ -0,0 +1,60 @@
+const estudiantes = [
+    {
+      id: 1,
+      nombre: "Paolo",
+      edad: 25,
+    },
+    {
+      id: 2,
+      nombre: "Jose",
+      edad: 20,
+    },
+    {
+      id: 3,
+      nombre: "Miguel",
+      edad: 25,
+    },
+    {
+      id: 4,
+      nombre: "Eduardo",
+      edad: 20,
+    },
+    {
+      id: 5,
+      nombre: "Raul",
+      edad: 20,
+    },
+  ];
+  
+  function filtarDatos () {
+    const datosMapeados = estudiantes.map((estudiante)=> {
+        return {
+            id:estudiante.id,
+            nombre: estudiante.nombre,
+        };
+    });
+
+    const datosFiltrados = estudiantes.filter (
+        (estudiante) => estudiantes.id >2
+    );
+
+    let alumnosMayoresA22 = 0;
+
+    const datosForeacheados = estudiantes.forEach((estudiante)=>{
+        if (estudiante.edad)>22 {
+            alumnosMayoresA22 = alumnosMayoresA22 +1;
+        }
+    });
+
+    const datosOrdenadosAlfabeticamente = estudiantes.sort ((a,b) => {
+        if (a.nombre < b.nombre){
+            return -1
+        } 
+        if (a.nombre > b.nombre){
+            return 1;
+    });
+
+    console.log(datosOrdenadosAlfabeticamente);
+  }
+
+  filtarDatos();
