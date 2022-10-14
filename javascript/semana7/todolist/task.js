82a3a4e5eff310d1d1e895d4a647b59d03d6965a
manue777 <lejuene.manu@gmail.com>
2022-10-13 21:00:13 -0500

    edit list

--- javascript/semana7/todolist/task.js
+++ javascript/semana7/todolist/task.js
@@ -21,30 +21,38 @@
 class Task {
   // Estos parametros
   constructor(id, name, date, status) {
-    this._id = id;
-    this._name = name;
-    this._date = date;
-    this._status = status;
+      this._id = id;
+      this._name = name;
+      this._date = date;
+      this._status = status;
   }
 
   static destroyRender(id) {
-    const element = document.querySelector(`#task-${id}`);
-    element.remove();
+      const element = document.querySelector(`#task-${id}`);
+      element.remove();
   }
 
-  // Es una clase no hace falta usar la palabra function
-  render() {
-    return `
-      <div id="task-${this._id}" class="item__task">
-        <input type="checkbox" />
-        <h6>${this._name}</h6>
-        <button>
-          <img src="./images/edit.png" width="15" alt="" />
-        </button>
-        <button onclick="destroy(${this._id})">
-          <img src="./images/delete.png" width="15" alt="" />
-        </button>
-      </div>
-      `;
+  static updateRender(id, name) {
+      const texto = document.querySelector(`#task-name-${id}`);
+      // Si quieren cambiar el texto de un elemento pueden usar
+      // * innerText => Antiguo
+      // * textContent => Moderno
+      texto.textContent = name;
   }
+
+// Es una clase no hace falta usar la palabra function
+render() {
+  return `
+    <div id="task-${this._id}" class="item__task">
+      <input type="checkbox" />
+      <h6 id="task-name-${this._id}">${this._name}</h6>
+      <button onclick="edit(${this._id})">
+        <img src="./images/edit.png" width="15" alt="" />
+      </button>
+      <button onclick="destroy(${this._id})">
+        <img src="./images/delete.png" width="15" alt="" />
+      </button>
+    </div>
+    `;
+}
 }
