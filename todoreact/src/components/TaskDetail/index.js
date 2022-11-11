a72786e88566ce3fe299f15b0653175f1a1b42c2
manue777 <lejuene.manu@gmail.com>
2022-11-10 21:59:44 -0500

    add authcontext

--- todoreact/src/components/TaskDetail/index.js
+++ todoreact/src/components/TaskDetail/index.js
@@ -1,34 +1,44 @@
-function TaskDetail(props) {
-	const { task } = props;
+import { useRef } from "react";
 
-	return (
-		<div className="card p-4 shadow-lg">
-			<h1>{task.name}</h1>
-			<hr />
-			<p>
-				<span className="fw-bold">Creación: &nbsp;</span>
-				<span className="text-muted">
-					{task.getDateTimeString(task.createdAt)}
-				</span>
-			</p>
-			{task.doneAt && (
-				<p className="text-success">
-					<span className="fw-bold">Realizado: &nbsp;</span>
-					<span className="opacity-50">
-						{task.getDateTimeString(task.doneAt)}
-					</span>
-				</p>
-			)}
-			{task.deletedAt && (
-				<p className="text-danger">
-					<span className="fw-bold">Eliminado: &nbsp;</span>
-					<span className="opacity-50">
-						{task.getDateTimeString(task.deletedAt)}
-					</span>
-				</p>
-			)}
-		</div>
-	);
+function TaskForm(props) {
+  const { onSubmitFunction } = props;
+
+  const inputElement = useRef();
+
+  function inputKeyPress(event) {
+    if (event.key === "Enter") {
+      addButton();
+    }
+  }
+
+  function addButton() {
+    if (inputElement.current.value === "") return;
+    onSubmitFunction(inputElement.current.value);
+    inputElement.current.value = "";
+  }
+
+  return (
+    <div className="card p-3 shadow">
+      <div className="input-group">
+        <input
+          id="input_task"
+          type="text"
+          className="form-control"
+          placeholder="My new task..."
+          ref={inputElement}
+          onKeyPress={inputKeyPress}
+        />
+        <button
+          className="btn btn-outline-primary"
+          type="button"
+          id="button-addon2"
+          onClick={addButton}
+        >
+          Add
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default TaskDetail;
+export default TaskForm;
