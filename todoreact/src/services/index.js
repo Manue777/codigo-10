3463716185c866a4a38a90dd57280daadc664192
manue777 <lejuene.manu@gmail.com>
2022-11-16 22:24:51 -0500

    books 2

--- todoreact/src/services/index.js
+++ todoreact/src/services/index.js
@@ -4,7 +4,7 @@ const urlBooks = "https://www.googleapis.com/books/v1/volumes?q=";
 
 export const getBooks = async (search) => {
   try {
-    const response = await fetch(`${urlBooks}${search}`);
+    const response = await fetch(`${urlBooks}${search}&maxResults=20`);
     const data = await response.json();
     return data;
   } catch (error) {
@@ -12,118 +12,117 @@ export const getBooks = async (search) => {
   }
 };
 
-
 // GET : Listar
 // POST: Crear
 // PUT: Actualizar
 // DELETE: Eliminar
 export const getProfile = async () => {
-	try {
-		const response = await fetch("https://api.github.com/users/linder3hs");
-		const data = await response.json();
-		return data;
-	} catch (error) {
-		console.log("Error", error);
-	}
+  try {
+    const response = await fetch("https://api.github.com/users/linder3hs");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Error", error);
+  }
 };
 
 export const get = async () => {
-	try {
-		const response = await fetch(url);
-		const data = await response.json();
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getById = async (id) => {
-	try {
-		const response = await fetch(url + "/" + id);
-		console.log(response);
-		if (response.ok) {
-			const data = await response.json();
-			return data;
-		} else {
-			return null;
-		}
-	} catch (error) {
-		console.log(error);
-		return null;
-	}
+  try {
+    const response = await fetch(url + "/" + id);
+    console.log(response);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
 
 export const post = async (body) => {
-	try {
-		const response = await fetch(url, {
-			method: "POST",
-			headers: {
-				"Content-type": "application/json",
-			},
-			body: JSON.stringify(body),
-		});
-		const data = await response.json();
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const postUser = async (user) => {
-	try {
-		const response = await fetch(urlUsers, {
-			method: "POST",
-			headers: {
-				"Content-type": "application/json",
-			},
-			body: JSON.stringify(user),
-		});
-		const data = await response.json();
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const response = await fetch(urlUsers, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getUsers = async () => {
-	try {
-		const response = await fetch(urlUsers);
-		const data = await response.json();
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const response = await fetch(urlUsers);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // id: Tarea
 export const update = async (id, body) => {
-	try {
-		const response = await fetch(`${url}/${id}`, {
-			method: "PUT",
-			headers: {
-				"Content-type": "application/json",
-			},
-			body: JSON.stringify(body),
-		});
-		const data = await response.json();
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const response = await fetch(`${url}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // Para la eliminar un registro unicamente necesito el id
 export const destroy = async (id) => {
-	try {
-		const response = await fetch(`${url}/${id}`, {
-			method: "DELETE",
-			headers: {
-				"Content-type": "application/json",
-			},
-		});
-		const data = await response.json();
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const response = await fetch(`${url}/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
