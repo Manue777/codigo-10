e5e7c8ada9501e8826d0cb7e0d9faa98b66b5c10
manue777 <lejuene.manu@gmail.com>
2022-11-16 22:01:25 -0500

    login material

--- todoreact/src/pages/Books/index.jsx
+++ todoreact/src/pages/Books/index.jsx
@@ -1,35 +1,53 @@
+import { useState } from "react";
 import {
-    Box,
-    Container,
-    Grid,
-    Card,
-    CardContent,
-    TextField,
-    Button,
-  } from "@mui/material";
-  
-  const Books = () => {
-    return (
-      <Container maxWidth="sm">
-        <Box mt={3}>
-          <Card>
-            <CardContent>
-              <Grid container spacing={3}>
-                <Grid item xs={12} md={9}>
-                  <TextField size="small" fullWidth label="Buscar libros" />
-                </Grid>
-                <Grid item xs={12} md={3}>
-                  <Button variant="contained" color="warning">
-                    Buscar
-                  </Button>
-                </Grid>
-              </Grid>
-            </CardContent>
-          </Card>
-        </Box>
-      </Container>
-    );
+  Box,
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  TextField,
+  Button,
+} from "@mui/material";
+
+const Books = () => {
+  const [search, setSearch] = useState("");
+
+  const handleInputChange = (e) => setSearch(e.target.value);
+
+  const handleSubmit = () => {
+    console.log(search);
   };
-  
-  export default Books;
-  
+
+  return (
+    <Container maxWidth="sm">
+      <Box mt={3}>
+        <Card>
+          <CardContent>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={9}>
+                <TextField
+                  onChange={handleInputChange}
+                  value={search}
+                  size="small"
+                  fullWidth
+                  label="Buscar libros"
+                />
+              </Grid>
+              <Grid item xs={12} md={3}>
+                <Button
+                  onClick={handleSubmit}
+                  variant="contained"
+                  color="warning"
+                >
+                  Buscar
+                </Button>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Box>
+    </Container>
+  );
+};
+
+export default Books;
