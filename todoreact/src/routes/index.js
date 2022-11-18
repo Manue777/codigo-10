19b6deea29e369c1a44294fe2b2e49b38771fb8d
manue777 <lejuene.manu@gmail.com>
2022-11-17 23:17:48 -0500

    Basket

--- todoreact/src/routes/index.js
+++ todoreact/src/routes/index.js
@@ -2,34 +2,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Pas2: Importar nuestras vistas
 import {
-	HomeView,
-	ProfileView,
-	TaskView,
-	LoginView,
-	SignUpView,
-	TodoAppView,
-	BooksView,
+  HomeView,
+  ProfileView,
+  TaskView,
+  LoginView,
+  SignUpView,
+  TodoAppView,
+  BooksView,
+  BasketView,
 } from "../pages";
 import { MainLayout } from "../layouts";
 
 // path => Nombre de la ruta /, /login, /registro
 
 const Router = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route element={<MainLayout />}>
-					<Route path="/" element={<HomeView />} />
-					<Route path="/books" element={<BooksView />} />
-					<Route path="/todoapp" element={<TodoAppView />} />
-					<Route path="/perfil" element={<ProfileView />} />
-					<Route path="/task/:id" element={<TaskView />} />
-				</Route>
-				<Route path="/login" element={<LoginView />} />
-				<Route path="/signup" element={<SignUpView />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/books" element={<BooksView />} />
+          <Route path="/basket" element={<BasketView />} />
+          <Route path="/todoapp" element={<TodoAppView />} />
+          <Route path="/perfil" element={<ProfileView />} />
+          <Route path="/task/:id" element={<TaskView />} />
+        </Route>
+        <Route path="/login" element={<LoginView />} />
+        <Route path="/signup" element={<SignUpView />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
